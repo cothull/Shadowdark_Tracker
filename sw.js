838ccfe48fd4addf545bd095e9a95a4c34578644
cothull <jackpcottle@gmail.com>
2026-02-12 15:57:32 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,34 @@
-const cacheName = 'v1';
+const cacheName = 'v2'; // Changed to v2 to force an update
 const cacheAssets = [
+  './',                // Caches the main folder/index
   'index.html',
+  'manifest.json',      // Crucial for PWA installation
   'combat_button.jpg',
-'grunge_left.png',
-'grunge_right.png',
-'paper_bg.jpg',
-'rations.jpg',
-'start.jpg',
-'icon.png',
+  'grunge_left.png',
+  'grunge_right.png',
+  'paper_bg.jpg',
+  'rations.jpg',
+  'start.jpg',
+  'icon.png',
   'torches.jpg'
 ];
 
+// Install Event
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(cacheAssets))
+    caches.open(cacheName).then(cache => {
+      console.log('Caching files...');
+      return cache.addAll(cacheAssets);
+    })
   );
 });
 
+// Fetch Event (Cache-First Strategy)
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      // Return cache hit, else fetch from network
+      return response || fetch(e.request);
+    })
+  );
 });
